7b1825de1a5e94b673c569789eb32a1a9acbaaee
yoon <luffy1021@naver.copm>
2020-08-31 10:08:26 +0900

    * Project Default Settings

---
+++ kbuniform/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kbuniform
+//
+//  Created by twave on 2020/08/31.
+//  Copyright © 2020 seokyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
