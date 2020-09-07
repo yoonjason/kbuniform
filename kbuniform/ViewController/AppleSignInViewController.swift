4e4a62c3b040680713a61ee7374eef27f888fe95
yoon <luffy1021@naver.copm>
2020-09-07 09:23:54 +0900

    * Sign in With Apple Base Setting

---
+++ kbuniform/ViewController/AppleSignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AppleSignInViewController.swift
+//  kbuniform
+//
+//  Created by twave on 2020/09/07.
+//  Copyright © 2020 seokyu. All rights reserved.
+//
+
+import UIKit
+
+class AppleSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- kbuniform/ViewController/LoginViewController.swift
+++ kbuniform/ViewController/LoginViewController.swift
@@ -12,75 +12,78 @@ import RxSwift
 import NSObject_Rx
 import LocalAuthentication
 
+
+
 class LoginViewController: UIViewController {
-    
+
+
     @IBOutlet weak var successStatusLabel: UILabel!
     @IBOutlet weak var loginBtn: UIButton!
-    let authContext = LAContext()
-    var message : String?
-    var error : NSError?
+    let authContext                   = LAContext()
+    var message: String?
+    var error: NSError?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loginBtn
             .rx
             .tap
-            .subscribe(onNext : {
+            .subscribe(onNext: {
                 self.requestLogin()
             })
             .disposed(by: rx.disposeBag)
-        self.successStatusLabel.text = "본인 인증해주세요."
+    self.successStatusLabel.text      = "본인 인증해주세요."
     }
-    
-    private func requestLogin(){
-        
+
+    private func requestLogin() {
+
         if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
-            
+
             switch authContext.biometryType {
             case .faceID:
-                message = "Face ID 인증해주세요."
-            case .touchID :
-                message = "Touch ID로 인증해주세요."
-            case .none :
-                message = "둘 다 안되니 로그인하십시오 X를 눌러 Joy를 표하십시오."
-                
+    message                           = "Face ID 인증해주세요."
+            case .touchID:
+    message                           = "Touch ID로 인증해주세요."
+            case .none:
+    message                           = "둘 다 안되니 로그인하십시오 X를 눌러 Joy를 표하십시오."
+
             }
-            
+
             authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: message!) { (isSUccess, error) in
                 if isSUccess {
                     print("인증성공")
                     DispatchQueue.main.async {
-                        self.successStatusLabel.textColor = .red
-                        self.successStatusLabel.text = "인증 성공"
+    self.successStatusLabel.textColor = .red
+    self.successStatusLabel.text      = "인증 성공"
                     }
-                    
-                }else {
-                    if let error = error {
+
+                } else {
+    if let error                      = error {
                         print(error.localizedDescription)
                     }
                 }
             }
-        }else {
+        } else {
             print("X를 눌러 Joy를 표하십시오.")
-            let errorDescripiton = error?.userInfo["NSLocalizedDescription"] ?? ""
+    let errorDescripiton              = error?.userInfo["NSLocalizedDescription"] ?? ""
             print(errorDescripiton)
-            
-            let alert = UIAlertController(title: "Authentication Required", message: message, preferredStyle: .alert)
-            weak var usernameTextField : UITextField!
+
+    let alert                         = UIAlertController(title: "Authentication Required", message: message, preferredStyle: .alert)
+            weak var usernameTextField: UITextField!
             alert.addTextField(configurationHandler: { textField in
-                textField.placeholder = "아이디 좀.."
-                usernameTextField = textField
+    textField.placeholder             = "아이디 좀.."
+    usernameTextField                 = textField
             })
-            weak var passwordTextField : UITextField!
+            weak var passwordTextField: UITextField!
             alert.addTextField(configurationHandler: { textField in
-                textField.placeholder = "비번도 좀 적어주시겠어요?"
-                textField.isSecureTextEntry = true
-                passwordTextField = textField
+    textField.placeholder             = "비번도 좀 적어주시겠어요?"
+    textField.isSecureTextEntry       = true
+    passwordTextField                 = textField
             })
             alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
             alert.addAction(UIAlertAction(title: "확인", style: .destructive, handler: { action in
 //                print(usernameTextField.text! + " & " + passwordTextField.text!)
-                
+
             }))
             DispatchQueue.main.async {
                 self.present(alert, animated: true, completion: nil)
